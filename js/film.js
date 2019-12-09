06ef88065c85ee6f46e39c470822613e4929c6db
Faina Harmash <faya.garmash.work@gmail.com>
2019-12-09 20:57:47 +0200

    added film.js

---
+++ js/film.js
@@ -0,0 +1,18 @@
+"use strict";
+const filmData = {
+    name: "Friends",
+    startYear: 1994,
+    endYear: 2004,
+    genre: ["comedy","romance"],
+    country: "USA",
+    runtime: "22 minutes",
+    language: "english",
+    seasons: 10,
+    budget = "null",
+    actors: "undefined",
+}
+filmData ['rating']= 5
+filmData.age ['18']= false
+filmData.age ['12']= true
+
+console.log(filmData);
