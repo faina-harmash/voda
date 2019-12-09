66eb3af0dd70aa5d0aa1f9ade429526cd5027fc5
Faina Harmash <faya.garmash.work@gmail.com>
2019-12-09 21:26:27 +0200

    added question have you watched this film

--- js/film.js
+++ js/film.js
@@ -8,11 +8,14 @@ const filmData = {
     runtime: "22 minutes",
     language: "english",
     seasons: 10,
-    budget = "null",
+    budget: "null",
     actors: "undefined",
 }
-filmData ['rating']= 5
-filmData.age ['18']= false
-filmData.age ['12']= true
+// filmData ['rating']= 5
+// filmData.age ['18']= false
+// filmData.age ['12']= true
 
+filmData.name = confirm ("have you watched this movie?")
+alert( filmData.name);
 console.log(filmData);
+
