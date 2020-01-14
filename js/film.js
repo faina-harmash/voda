7f807bd7a562630d3622df00da001cc9e9d91cbf
Faina Harmash <faya.garmash.work@gmail.com>
2020-01-14 18:34:44 +0200

    switch function

--- js/film.js
+++ js/film.js
@@ -21,7 +21,7 @@ console.log(filmData);
 addRatePush(4);
 addRateConcat(7);
 
-console.log("Done")
+console.log("Done");
 
 function setNewRate(rate) {
   filmData.rate = rate;
@@ -51,43 +51,75 @@ function calculateSum(...rates) {
 
 function calculateAverage() {}
 
-// const myNumber = 5;
-// console.log(myNumber);
+//Function Change rating
+const myNumber = 5;
+console.log(myNumber);
 
-// filmData.isWatched = confirm ("have you watched this movie?")
-// alert( filmData.isWatched);
-// console.log(filmData);
+filmData.isWatched = confirm("have you watched this movie?");
+alert(filmData.isWatched);
+console.log(filmData);
 
-
-// 1. Создать функцию которая принимает в качестве аргумента рейтинг, а возвращает строку с сообщением. На каждую цифру рейтинга нужно придумать уникальное сообщение. Чем больше сообщений, тем лучше.
+// 1. Создать функцию которая принимает в качестве аргумента рейтинг, а возвращает строку с сообщением.
+//На каждую цифру рейтинга нужно придумать уникальное сообщение. Чем больше сообщений, тем лучше.
 // getRecommendation(5) // ‘Оличный фильм, нужно смотреть!’
 // getRecommendation(1) // ‘Фильм полное дно, смотреть не стоит!’
 
 let rate = prompt("How much did you like the movie", "10");
 function getRecommendation(rate) {
-if (rate==1) {
-return "Хочешь потратить время зря, тогда этот фильм точно для тебя!";
-} else if (rate==2) {
-return "Нужно дома убрать и ты ищешь фон, welcome to watch this!";
-} else if (rate==3) {
-return "Хочешь заснуть, welcome!";
-} else if (rate==4) {
-return "Можешь пролистать Instagram пока смотришь!";
-} else if (rate==5) {
-return "Где-то между 'Поле чудес' и 'Сумерками'!";
-}else if (rate==6) {
-return "Вот это актерский состав!";
-}else if (rate==7) {
-return "Однако хорошо снято!";
-}else if (rate==8) {
-return "Вот это сюжет!";
-}else if (rate==9) {
-return "Какой атмосферный фильм!";
-}else if (rate==10) {
-return "А можно ещё раз пересмотреть!";
+  if (rate == 1) {
+    return "Хочешь потратить время зря, тогда этот фильм точно для тебя!";
+  } else if (rate == 2) {
+    return "Нужно дома убрать и ты ищешь фон, welcome to watch this!";
+  } else if (rate == 3) {
+    return "Хочешь заснуть, welcome!";
+  } else if (rate == 4) {
+    return "Можешь пролистать Instagram пока смотришь!";
+  } else if (rate == 5) {
+    return "Где-то между 'Поле чудес' и 'Сумерками'!";
+  } else if (rate == 6) {
+    return "Вот это актерский состав!";
+  } else if (rate == 7) {
+    return "Однако хорошо снято!";
+  } else if (rate == 8) {
+    return "Вот это сюжет!";
+  } else if (rate == 9) {
+    return "Какой атмосферный фильм!";
+  } else if (rate == 10) {
+    return "А можно ещё раз пересмотреть!";
+  }
 }
+
+alert(getRecommendation(rate));
+
+//Конструкция "switch"
+let rate = prompt("How much did you like the movie", "From 1 to 10");
+
+const message = getRecommendationSwitch(rate);
+alert(message);
+
+function getRecommendationSwitch(rateSwitch) {
+  switch (rateSwitch) {
+    case "1":
+      return "Хочешь потратить время зря, тогда этот фильм точно для тебя!";
+    case "2":
+      return "Нужно дома убрать и ты ищешь фон, welcome to watch this!";
+    case "3":
+      return "Хочешь заснуть, welcome!";
+    case "4":
+      return "Можешь пролистать Instagram пока смотришь!";
+    case "5":
+      return "Где-то между 'Поле чудес' и 'Сумерками'!";
+    case "6":
+      return "Вот это актерский состав!";
+    case "7":
+      return "Однако хорошо снято!";
+    case "8":
+      return "Вот это сюжет!";
+    case "9":
+      return "Какой атмосферный фильм!";
+    case "10":
+      return "А можно ещё раз пересмотреть!";
+    default:
+      return "Smile";
+  }
 }
-
-
-
-alert (getRecommendation(rate));
