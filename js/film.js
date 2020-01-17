e3b371c6c61e3930d63ed1fb04af8cb947467cec
Faina Harmash <faya.garmash.work@gmail.com>
2020-01-17 19:18:50 +0200

    fixed function getRecommendationSwitch

--- js/film.js
+++ js/film.js
@@ -92,34 +92,48 @@ function getRecommendation(rate) {
 alert(getRecommendation(rate));
 
 //Конструкция "switch"
-let rate = prompt("How much did you like the movie", "From 1 to 10");
+let rate = +prompt("How much did you like the movie", "From 1 to 10");
 
 const message = getRecommendationSwitch(rate);
 alert(message);
 
 function getRecommendationSwitch(rateSwitch) {
-  switch (rateSwitch) {
-    case "1":
+  let numb = Math.floor(rateSwitch);
+  switch (numb) {
+    case 1:
       return "Хочешь потратить время зря, тогда этот фильм точно для тебя!";
-    case "2":
+    case 2:
       return "Нужно дома убрать и ты ищешь фон, welcome to watch this!";
-    case "3":
+    case 3:
       return "Хочешь заснуть, welcome!";
-    case "4":
+    case 4:
       return "Можешь пролистать Instagram пока смотришь!";
-    case "5":
+    case 5:
       return "Где-то между 'Поле чудес' и 'Сумерками'!";
-    case "6":
+    case 6:
       return "Вот это актерский состав!";
-    case "7":
+    case 7:
       return "Однако хорошо снято!";
-    case "8":
+    case 8:
       return "Вот это сюжет!";
-    case "9":
+    case 9:
       return "Какой атмосферный фильм!";
-    case "10":
+    case 10:
       return "А можно ещё раз пересмотреть!";
     default:
-      return "Smile";
+      return "Нужно ввести число от 1 до 10, для получения совета по фильму!";
+  }
+}
+
+//Написать функцию play, которая принимает 3 аргумента. Работать должна следующим образом:
+let length = 10;
+let phrase = "Боги джаваскрипта, сжальтесь";
+let startPhrase = 4;
+
+console.log(length);
+for (let i = 1; i < length; i++) {
+  console.log(i);
+  if (i % period == 0) {
+    console.log(phrase);
   }
 }
