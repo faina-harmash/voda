f4716d0867bb21b123543293177cb8004472ca63
Faina Harmash <faya.garmash.work@gmail.com>
2020-01-24 18:59:04 +0200

    added function makeCard

--- js/film.js
+++ js/film.js
@@ -149,7 +149,7 @@ function transformString(sentense) {
 }
 transformString("Ладушки и варенье");
 
-//////////////
+//the same but without function
 let smak = "Ладушки и варенье";
 let transform = smak.split("");
 let result = transform.join("-");
@@ -250,7 +250,7 @@ function lowerOrUpper(letters) {
 let letters = "javascript";
 lowerOrUpper(letters);
 
-/// 1 - 2firstletters and 2 - 2secondletters
+//1 - 2firstletters and 2 - 2secondletters
 function getTogether(firstWord, secondWord) {
   let result = "";
   result = firstWord.slice(0, 2) + secondWord.slice(-2);
@@ -261,7 +261,7 @@ let firstWord = "Пташка";
 let secondWord = "Пiвник";
 getTogether(firstWord, secondWord);
 
-//// let the same but with if the word has less than two letters return whole word
+//let the same but with if the word has less than two letters return whole word
 
 function getTogether(firstWord, secondWord) {
   let result = "";
@@ -275,5 +275,120 @@ function getTogether(firstWord, secondWord) {
   return result;
 }
 let firstWord = "ш";
-let secondWord = "Пiвник";
+let secondWord = "u";
 getTogether(firstWord, secondWord);
+
+/*Написать функцию makeCard. 
+Она принимает 3 аргумента и возвращает объект как на скриншоте.
+Месяц должен быть выбран случайно при запуске функции и всегда состоять из двух чисел. 
+Год должен быть выбран случайно из таких чисел 30, 29, 28, 34.
+CVV должен всегда состоять из 3 чисел и быть случайным.
+Если в функцию не передать имени и фамилии она будет без имени.*/
+
+//Написать функцию makeCard. Она принимает 3 аргумента и возвращает объект как на скриншоте.
+function makeCard(cardType, firstName, secondName) {
+  let cardInfo = {};
+  cardInfo.type = cardType.toUpperCase();
+  cardInfo.name = firstName.toUpperCase() + " " + secondName.toUpperCase();
+
+  return cardInfo;
+}
+
+makeCard("VISA", "Faina", "Harmash");
+
+//Если в функцию не передать имени и фамилии она будет без имени.*/
+function makeCard(cardType, firstName, secondName) {
+  let cardInfo = {};
+  cardInfo.type = cardType.toUpperCase();
+
+  if (firstName !== undefined && secondName !== undefined) {
+    cardInfo.name = firstName.toUpperCase() + " " + secondName.toUpperCase();
+  }
+  return cardInfo;
+}
+
+makeCard("VISA");
+
+//Месяц должен быть выбран случайно при запуске функции и всегда состоять из двух чисел.
+
+function randomMonth(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let monthNumber = randomMonth(1, 12);
+
+function makeCard(cardType, firstName, secondName) {
+  let cardInfo = {};
+  cardInfo.type = cardType.toUpperCase();
+  cardInfo.month = monthNumber;
+
+  if (firstName !== undefined && secondName !== undefined) {
+    cardInfo.name = firstName.toUpperCase() + " " + secondName.toUpperCase();
+  }
+  return cardInfo;
+}
+
+makeCard("VISA", "Faina", "Harmash");
+
+//CVV должен всегда состоять из 3 чисел и быть случайным.
+//CVV function
+
+function randomCvv() {
+  return Math.floor(Math.random() * 900) + 100;
+}
+let cvvNumber = randomCvv();
+
+function makeCard(cardType, firstName, secondName) {
+  let cardInfo = {};
+  cardInfo.type = cardType.toUpperCase();
+  cardInfo.cvv = cvvNumber;
+
+  if (firstName !== undefined && secondName !== undefined) {
+    cardInfo.name = firstName.toUpperCase() + " " + secondName.toUpperCase();
+  }
+  return cardInfo;
+}
+
+makeCard("VISA", "Faina", "Harmash");
+
+//Год должен быть выбран случайно из таких чисел 30, 29, 28, 34.
+
+function makeCard(cardType, firstName, secondName) {
+  let cardInfo = {};
+  cardInfo.type = cardType.toUpperCase();
+  if (firstName !== undefined && secondName !== undefined) {
+    cardInfo.name = firstName.toUpperCase() + " " + secondName.toUpperCase();
+  }
+  cardInfo.year = randomYear();
+  cardInfo.cvv = randomCvv();
+  cardInfo.month = randomMonth();
+  return cardInfo;
+}
+
+function randomNumber(min, max) {
+  let randomPart = Math.random() * (max - min);
+  console.log(randomPart);
+  return Math.floor(randomPart) + min;
+}
+
+function randomYear() {
+  let yearArray = [30, 29, 28, 34];
+  let randomIndex = randomNumber(0, yearArray.length);
+  let yearNumber = yearArray[randomIndex];
+  return yearNumber;
+}
+
+function randomCvv() {
+  let cvv = "";
+  cvv = cvv.concat(randomNumber(0, 10));
+  cvv = cvv.concat(randomNumber(0, 10));
+  cvv = cvv.concat(randomNumber(0, 10));
+  return cvv;
+}
+
+function randomMonth(){
+  let month = "";
+  month = (randomNumber(1,12));
+  return month;
+}
+
+makeCard("VISA", "Faina", "Harmash");
